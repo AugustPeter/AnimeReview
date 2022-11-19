02c0f149432cc12b84217146e9362cac7173ffd4
AugustPeter <Pedro4ugusto14@gmail.com>
2022-11-19 13:18:38 -0300

    Co-authored-by: BarbosaLuke <BarbosaLuke@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
 const burgerMenu = document.querySelector('.burger')
 const ul = document.querySelector('ul')
+const modal = document.querySelector('.modal')
+const perfil = document.querySelector('#perfil')
+const closeModal = document.querySelector('.close')
+const lis = document.querySelectorAll('li')
+const header = document.querySelector('header')
+const user = document.querySelector('.user')
 
-burgerMenu.addEventListener('onmouseover', () =>{
-  
+
+
+
+perfil.addEventListener('click', () =>{
+    modal.classList.add('active')
+})
+closeModal.addEventListener('click',() =>{
+    modal.classList.remove('active')
+})
+
+burgerMenu.addEventListener('click',() =>{
+    header.classList.toggle('active')
+    ul.classList.toggle('active')
+
+    for (const li of lis) {
+        li.classList.toggle('active');
+      }
+
 })
